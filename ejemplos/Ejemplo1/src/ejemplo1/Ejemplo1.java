9f240b1b2afce31cfc65c464939f7ecff955986f
unknown <sjmayon1@utpl.edu.ec>
2023-11-08 15:34:58 -0500

    ejercicio2

--- ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
+++ ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -22,17 +22,17 @@ public class Ejemplo1 {
         if (promedio >= 7.5) {
             System.out.printf("Estudiante aprobado con un "
                     + "promedio: %.2f\n", promedio);
-        }
+        }//finalizacion de la linea 22
 
-        if (promedio > 7.5) {
+        if (promedio == 7.5) {
             System.out.printf("Estudiante aprobado con un "
                     + "promedio: %.2f\n", promedio);
-        }
+        }//finalizacion de la liena 27
 
         if (promedio < 7.5) {
             System.out.printf("Estudiante aprobado con un "
                     + "promedio: %.2f\n", promedio);
-        }
+        }//finalizacion de la linea del metodo main 
     }
 
 }
