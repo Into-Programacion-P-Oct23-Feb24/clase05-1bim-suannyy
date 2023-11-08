b1069ef655e4bbc65c7d899dfdffdae700cece5d
unknown <sjmayon1@utpl.edu.ec>
2023-11-08 16:29:36 -0500

    ejemplo3

--- ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
+++ ejemplos/Ejemplo3/src/ejemplo3/Ejemplo3.java
@@ -20,6 +20,11 @@ public class Ejemplo3 {
 
     public static void main(String[] args) {
         // Condicionales aninados
+        // >= 7.5 Aprobado
+        // <= 5 and <7.5 Suspenso
+        // >= 3 and < 5 Recuperacion
+        // < 3 Reprobado
+       
         double promedio = 2;
 
         if (promedio >= 7.5) {
@@ -30,9 +35,17 @@ public class Ejemplo3 {
                 System.out.printf("Estudiante en suspenso con un "
                         + "promedio: %.2f\n", promedio);
             } else {
-                System.out.printf("Estudiante reprobado con un "
+                if ((promedio >= 3) && (promedio < 5)) {
+                System.out.printf("Estudiante recuperacion con un "
                         + "promedio: %.2f\n", promedio);
-            }
+            
+                }  else {
+                    if (promedio < 3){
+                    System.out.printf("Estudiante reprobado con un "  
+                          + "promedio: %.2f\n", promedio);  
+                    } 
+                }
+            } 
 
         }
     }
